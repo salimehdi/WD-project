f88d32b5f2cc5e0779d1df0db607eb39bd6118bb
Ayaan Shaikh <ayaanmaths04@gmail.com>
2024-04-25 22:13:25 +0530

    Add inventory header styles and buttons

--- app/home/inventory/page.tsx
+++ app/home/inventory/page.tsx
@@ -1,7 +1,61 @@
+import './header.css';
+
 export default function Page() {
     return (
-        <div className="w-[100%] h-[100%] bg-blue-100">
-            <p>Inventory Page</p>
+        <div className="w-[100%] h-[100%]">
+            <header>
+                <h1 className="text-4xl text-blue-900 opacity-50 font-bold">
+                    Inventory
+                    <span className="text-xl opacity-50 mx-4">{126} items listed</span>
+                </h1>
+                <div className="buttons">
+                    <div className="ctas">
+                        <button>+ Add new stock</button>
+                        <button>+ Add old stock</button>
+                    </div>
+                    <button className="barcode-btn">Generate Barcode</button>
+                </div>
+                <table className='w-full h-full bg-blue-100 text-left mx-2 rounded-lg'>
+                    <thead>
+                        <tr className='text-blue-900 opacity-60'>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Brand Name</th>
+                            <th>Quantity</th>
+                            <th>Status</th>
+                            <th>Item cost</th>
+                            <th>Estimated price</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1.</td>
+                            <td>Cricket Bat Small Size</td>
+                            <td>MRF bats</td>
+                            <td>87 N</td>
+                            <td>Low</td>
+                            <td>1000 ₹</td>
+                            <td>1200 ₹</td>
+                            <td>
+                                Menu
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2.</td>
+                            <td>Cricket Bat Small Size</td>
+                            <td>MRF bats</td>
+                            <td>87 N</td>
+                            <td>Out</td>
+                            <td>1000 ₹</td>
+                            <td>1200 ₹</td>
+                            <td>
+                                Menu
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </header>
         </div>
     );
 }
